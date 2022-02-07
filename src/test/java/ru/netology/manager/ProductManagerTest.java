f0a576f9f0f67c82aa8db789a7f24f79688c09c2
Darya9810 <93486920+Darya9810@users.noreply.github.com>
2022-02-07 17:02:55 +0300

    ProductManager

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -10,12 +10,12 @@ import ru.netology.repo.ProductRepository;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerTest {
-    private final Product firstProduct = new Product(1, "firstProduct_me", 1);
-    private final Product secondProduct = new Product(2, "secondProduct_Me", 2);
-    private final Book firstBook = new Book(3, "firstBook", 3, "me");
-    private final Book secondBook = new Book(4, "secondBook", 4, "you");
-    private final Smarthone firstSmarthone = new Smarthone(5, "firstSmarthone", 5, "me");
-    private final Smarthone secondSmarthone = new Smarthone(6, "secondSmarthone", 6, "you");
+    private final Product firstProduct = new Product(1, "телевизор", 50000);
+    private final Product secondProduct = new Product(2, "радио", 1000);
+    private final Book firstBook = new Book(3, "Теремок", 120, "Булатов");
+    private final Book secondBook = new Book(4, "Колобок", 250, "Ушинский");
+    private final Smarthone firstSmarthone = new Smarthone(5, "Apple-iPhone", 100000, "Джон Кейси");
+    private final Smarthone secondSmarthone = new Smarthone(6, "Xiaomi ", 20000, "Лэй Цзюнь");
 
     private ProductManager manager = new ProductManager(new ProductRepository());
 
@@ -30,10 +30,38 @@ class ProductManagerTest {
     }
 
     @Test
-    void shouldReturnByText() {
-        Product[] expected = {firstProduct, firstBook, firstSmarthone};
-        Product[] actual = manager.searchBy("me");
+    void shouldReturnByTextProduck() {
+        Product[] expected = {firstProduct};
+        Product[] actual = manager.searchBy("телевизор");
 
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnByTextBook() {
+        Product[] expected = {firstBook};
+        Product[] actual = manager.searchBy("Булатов");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnBySmarthone() {
+        Product[] expected = {firstSmarthone};
+        Product[] actual = manager.searchBy("Джон Кейси");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldNotFindAuthor() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Лэй Цзюнь");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindNull() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy(null);
         assertArrayEquals(expected, actual);
     }
 }
